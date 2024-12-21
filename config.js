552b048ea4a8d3f4d2779ff46544e0a4f1d4cf95
Ali Ahmad Farooq <aliahmad747426@gmail.com>
2024-12-21 20:39:17 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "y2ZVxTrA#LGceAnMd5E-lS7SSx-SA5NFQLuOC070lCzmPRI_9jY8",
+SESSION_ID: process.env.SESSION_ID || "TuwkEbbb#WtsBqZxbELfYGMvxvA3F8Cy4WJE0HNrsMJ3xL3IV2C4",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE  === undefined ? 'false' : process.env.AUTO_VOICE, 
@@ -17,7 +17,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || "$",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "true",
-BOT_NAME: process.env.BOT_NAME || "❖ꜱʜᴇɪᴋʜ-ᴀʟɪ-ᴍᴅ❖",
+BOT_NAME: process.env.BOT_NAME || "✦✦ꜱʜᴇɪᴋʜ-ᴀʟɪ-ᴍᴅ✦✦",
 };
